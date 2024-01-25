510de22698da75027ddab111df57fd4ebba04a27
Abhishek Maurya <abhimaur.live@gmail.com>
2024-01-26 02:21:44 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants";
 import connectDB from "./db/index.js"
-import express from "express"
 import dotenv from "dotenv"
-const app = express()
 dotenv.config({
     path:'./env'
 })
 
-connectDB()
+connectDB() // this returns a promise
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}, if not, then check port:8000`)
+    })
+})
+.catch((error)=>{
+    console.log("App connection failed! ", error);
+})
 
 //Approach 1: By IIFE
 /* ( async (  )=>{
